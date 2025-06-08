6a35d29aa4467164d439492a787b819855ff7a5b
Sagarkumarsoni <sagarkrsoni218@gmail.com>
2025-06-08 20:00:24 +0530

    Added Express Sessions

--- classroom/server.js
+++ classroom/server.js
@@ -2,13 +2,47 @@ const express = require("express");
 const app = express();
 const users = require("./routes/user.js");
 const posts = require("./routes/post.js");
+const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+// const cookieParser = require("cookie-parser");
 
-app.get("/", (req, res) => {
-    res.send("Hi, I am root");
+const sessionOptions = {secret: "mysupersecretstring", resave: false, saveUninitialized: true};
+
+app.set("views", path.join(__dirname, "views"));//
+app.set("view engine", "ejs");
+
+app.use(session(sessionOptions));
+app.use(flash());
+app.use((req, res, next) => {
+    res.locals.successMsg = req.flash("success");
+    res.locals.errorMsg = req.flash("error");
+    next();
+})
+
+app.get("/register", (req, res) => { //information is stored in the session
+    let {name = "anonymous"} = req.query;
+    req.session.name = name;
+    if(name == "anonymous") {
+        req.flash("error", "user not registered!");
+    } else {
+        req.flash("success", "user registered successfully!");
+    }
+    res.redirect("/hello");
 });
 
-app.use("/users", users);//mapping gets done for users router to /users
-app.use("/posts", posts);
+app.get("/hello", (req, res) => { //information is accessed from the session
+    res.render("page.ejs", {name: req.session.name});
+});
+
+// app.get("/reqcount", (req, res) => {
+//     if(req.session.count) {
+//         req.session.count++;
+//     } else {
+//         req.session.count = 1;
+//     }
+//     res.send(`You sent a request ${req.session.count} times`);
+// });
 
 app.listen(3000, () => {
     console.log("server is listening to port 3000");
--- routes/listing.js
+++ routes/listing.js
@@ -30,7 +30,12 @@ router.get("/new", (req, res) => {
 router.get("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id).populate("reviews");
-    res.render("listings/show.ejs", {listing});
+    if(! listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        res.redirect("/listings");
+    } else {
+        res.render("listings/show.ejs", {listing});
+    }
 }));
 
 //Create Route
@@ -43,8 +48,8 @@ router.post("/", validateListing, wrapAsync(async (req, res) => {
     const listing = req.body.listing; //request.body se listing object ko nikal rhe hai
     const newListing = new Listing(listing);
     // console.log(newListing);
-
     await newListing.save();
+    req.flash("success", "New Listing Created!");
     res.redirect("/listings")
 }));
 
@@ -52,13 +57,19 @@ router.post("/", validateListing, wrapAsync(async (req, res) => {
 router.get("/:id/edit", wrapAsync(async (req, res) => {
     let { id } = req.params;
     const listing = await Listing.findById(id);
-    res.render("listings/edit.ejs", { listing })
+    if(!listing) {
+        req.flash("error", "Listing you requested for does not exist!");
+        res.redirect("/listings");
+    } else {
+        res.render("listings/edit.ejs", { listing })
+    }
 }));
 
 //Update Route
 router.put("/:id", validateListing, async (req, res) => {
     let { id } = req.params;
     await Listing.findByIdAndUpdate(id, {...req.body.listing});//req.body.listing is a js object which has different parameters and we deconstruct and convert into individual values and pass it to the new updated value
+    req.flash("success", "Review Updated!");
     res.redirect(`/listings/${id}`);
 });
 
@@ -67,6 +78,7 @@ router.delete("/:id", wrapAsync(async (req, res) => {
     let { id } = req.params;
     let deletedListings = await Listing.findByIdAndDelete(id);
     console.log(deletedListings);
+    req.flash("success", "Listing Deleted!")
     res.redirect("/listings");
 }));
 
